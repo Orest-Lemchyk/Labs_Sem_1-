83385a3a3bf270048dbf974ec37d0adbe5035856
@Turbinka <orestlema@gmail.com>
2024-09-11 18:26:33 +0300

    11.09.2024

---
+++ Sport.py
@@ -0,0 +1,19 @@
+#Лабораторні роботи з дисципліни "Алгоритмізація та програмування"
+#Виконав: Лемчик Орест Юрійович (ІР-14)
+#Лабораторна робота №1 (Варіант 11)
+#Створити змінну присвоїти їй назву і вивести рядок. Порахувати вираз
+
+sport = "Table tenis"
+print(sport)
+
+is_student , is_teacher = False , True
+print(is_student)
+
+is_hot , is_cold = True , False
+result = is_hot or is_cold
+print(result)
+
+import math
+x, y = 15.241 , 7.118
+result = (3 * math.sqrt(x + y**3)) / (y**(1/3)) + (3 / (4 * x)) * math.cos(y)
+print(result)
