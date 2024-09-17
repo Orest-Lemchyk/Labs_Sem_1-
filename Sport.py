cb5530a217a43602878789807b774296400f5e0d
Orest-Lemchyk <orestlema@gmail.com>
2024-09-17 13:22:16 +0300

    v.2 code

--- Sport.py
+++ Sport.py
@@ -1,19 +1,19 @@
 #Лабораторні роботи з дисципліни "Алгоритмізація та програмування"
-#Виконав: Лемчик Орест Юрійович (ІР-14)
-#Лабораторна робота №1 (Варіант 11)
-#Створити змінну присвоїти їй назву і вивести рядок. Порахувати вираз
+ #Виконав: Лемчик Орест Юрійович (ІР-14)
+ #Лабораторна робота №1 (Варіант 11)
+ #Створити змінну присвоїти їй назву і вивести рядок. Порахувати вираз.
+
+from math import cos as cosinus , sqrt
 
 sport = "Table tenis"
 print(sport)
 
-is_student , is_teacher = False , True
-print(is_student)
+is_student, is_teacher = False, True
+print(is_student and not is_teacher)
 
-is_hot , is_cold = True , False
-result = is_hot or is_cold
-print(result)
+is_hot, is_cold = True, False
+print(is_hot or is_cold)
 
-import math
-x, y = 15.241 , 7.118
-result = (3 * math.sqrt(x + y**3)) / (y**(1/3)) + (3 / (4 * x)) * math.cos(y)
+x, y = 15.241, 7.118
+result = (3 *sqrt(x + y**3)) / (y ** (1 / 3)) + (3 / (4 * x)) * cosinus(y)
 print(result)
